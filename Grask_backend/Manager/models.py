3eb2a96af2fda1f82460fd3578f8cc107086339d
Simone-Damico <simodamico1992@gmail.com>
2020-04-10 18:47:37 +0200

    Migrate Grabit model

--- Grask_backend/Manager/models.py
+++ Grask_backend/Manager/models.py
@@ -1,14 +1,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils import timezone
 from jsonfield import JSONField
 
 
 # Create your models here.
 
 
-class Project(models.Model):
+class Grabit(models.Model):
 
     DBMS = (('MySQL', 'MySQL'), ('SQLite', 'SQLite'), ('Neo4j', 'Neo4j'))
 
@@ -19,7 +18,8 @@ class Project(models.Model):
     description = models.TextField(blank=True, null=True)
     port = models.IntegerField()
     folder = models.FilePathField()
-    created_date = models.DateField(default=timezone.now, auto_now=False)
+    created_date = models.DateField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
     graph = JSONField(null=True)
 
     #p = Project(name_project='Manager', name_db='ManagerDB', dbms='SQLite', description='Database del manager del sistema', port=8000, folder='C:\\Users\\Utente\\Desktop\\django\\serverdb\\manager', graph=None)
@@ -27,3 +27,4 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name_project
+
