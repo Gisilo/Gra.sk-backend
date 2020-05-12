399ca82f73809925a4ea88db998ef1274d995fff
Giuseppe <depalma.gsp@gmail.com>
2020-05-12 11:55:15 +0200

    Production environment variables and fix debug bool read in settings

--- Grask_backend/settings.py
+++ Grask_backend/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG", default=False)
+DEBUG = os.environ.get("DEBUG", default=False) == "True"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
