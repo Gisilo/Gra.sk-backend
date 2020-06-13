1844cc5f9dbdadaa5cdd1e5faccd9264f9779ff5
giusdp <depalma.gsp@gmail.com>
2020-06-13 10:06:23 +0200

    Add me query to test JWT and put back csrf middleware

--- grask/settings.py
+++ grask/settings.py
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware', TODO
+    'django.middleware.csrf.CsrfViewMiddleware',  # TODO
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
