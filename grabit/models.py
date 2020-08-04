9c63227d978ce345e5659b51067b5597eb95f82e
Simone-Damico <simodamico1992@gmail.com>
2020-08-04 16:45:55 +0200

    Change dateTime field in grabit model

--- grabit/models.py
+++ grabit/models.py
@@ -13,8 +13,8 @@ class Grabit(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    creation_date = models.DateField(auto_now_add=True)
-    update_date = models.DateField(auto_now=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
     graph = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
